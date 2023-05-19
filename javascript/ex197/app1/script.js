46223ea284c1e0cd4672a61ecd791821c7d5ef54
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2023-05-19 05:55:06 -0300

    exercicio de form

--- javascript/ex197/app1/script.js
+++ javascript/ex197/app1/script.js
@@ -7,11 +7,15 @@ function calcular(tipo, valor) {
             //limpar o visor (id resultado)
         document.getElementById("resultado").value = ''
         }
-        if(valor === '+' ) {
+        if( valor === '+' || valor === '-' || valor === "*" || valor === '/' || valor === '.') {
             document.getElementById("resultado").value += valor
         }
+        if ( valor === '=') {
+            var valor_campo = eval(document.getElementById("resultado").value)
+            //console.log(eval(valor_campo))
 
-        
+            document.getElementById('resultado').value = valor_campo
+        }     
 
     } else if(tipo === 'valor') {
         //var valor_campo = document.getElementById("resultado").value
