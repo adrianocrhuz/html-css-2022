7fe5bf8942eaea8e8993449e6146dfb61558955f
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2023-09-16 07:08:42 -0300

    jogo

--- javascript/ex228/app2/jogo.js
+++ javascript/ex228/app2/jogo.js
@@ -1,3 +1,4 @@
+//definindo a dimensão do palco do jogo, encontra a altura e largura
 let altura = 0;
 let largura = 0;
 
@@ -11,37 +12,40 @@ ajustaTamanhoPalcoJogo()
 // função randomica para a img
 
 function posicaoRandomica() {
-    let posicaoX = Math.floor(Math.random()* altura) - 90;
-    let posicaoY = Math.floor(Math.round()* largura) - 90;
+    let posicaoX = Math.floor(Math.random() * altura) - 90;
+    let posicaoY = Math.floor(Math.round() * largura) - 90;
 
     //Criar operador ternário
     posicaoX = posicaoX < 0 ? 0 : posicaoX;
     posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-    //criar o elemento da img
+    //criar o elemento da html (img)
 
     let mosquito = document.createElement('img');
     // adicionar a imagens via js
     //adicionar img
     mosquito.src = "imagens/mosquito.png"
 
-    //criar os estilos do css
-    mosquito.className = tamanhoAleatotio() //'mosquito1'
+    //criar os estilos do css (img)
+    mosquito.className = tamanhoAleatotio() + ' ' + ladoAleatorio()
 
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
+    //incluindo a img de forma programatica no body 
     document.body.appendChild(mosquito);
 
     console.log(ladoAleatorio())
 
     //console.log(tamanhoAleatotio())
 }
-    //tamanho aleatório do mosquito
+    //tamanho aleatório do mosquito (img)
     function tamanhoAleatotio() {
         let classe = Math.floor(Math.random()* 3)
         //console.log(classe)
+
+        //definir o tamanho dos mosquitos
 
         switch(classe) {
             case 0:
@@ -52,7 +56,7 @@ function posicaoRandomica() {
                 return 'mosquito3'
         }
     }
-    // lado a lado
+    // lado a lado posicionamento do mosquito aleatoriamente
     function ladoAleatorio() {
         let classe = Math.floor(Math.random() * 2)
 
